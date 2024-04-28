c63d30a2b08971b5dfedecd7cf6e867e7625d542
yohandevperera <yohanperera27@gmail.com>
2024-04-28 08:18:21 +0530

    added db connection

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
+include("./utils/db-connection.php"); 
+	
 echo("Hello world");
 ?>
--- utils/db-connection.php
+++ utils/db-connection.php
@@ -1,4 +1,14 @@
 <?php
 
-
+function getConnectionInstance()
+{
+    try {
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        return (mysqi_connect($host, $username, $password));
+    } catch (Exception $e) {
+        echo ($e);
+    }
+}
 ?>
