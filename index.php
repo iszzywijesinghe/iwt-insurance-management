5cd2b9b1f97dc2e50a67633fe44c88312d7082e8
Kasunianupama <kasunianupama509@gmail.com>
2024-04-29 15:34:17 +0530

    updated index file

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <meta content="" name="description">
 
         <!-- Template Stylesheet -->
-        <link href="./asserts/css/styles.css" rel="stylesheet">
+        <link href="./asserts/css/Regstylesheet.css" rel="stylesheet">
 
 
  
@@ -24,46 +24,95 @@
         <link rel="stylesheet" href="Styles.css">
     </head>
     <body>
-        <div class="top">
-            <div class="heading1">
-                <h2>Welcome!</h2>
-                <h1>Get Your Shield Today</h1>
+    <nav>
+            <div class="logo">
+              <img src="your-logo.png" alt="Logo">
             </div>
-            <div class="buttons">
-                <button type="button" id="loginBtn">Login</button>
-                <p class="text">or</p>
-                <button type="button" id="signupBtn">Sign Up</button>
-            </div>
+            <ul>
+              <li><a href="Home.html">Home</a></li>
+              <li><a href="About Us.html">About Us</a></li>
+              <li><a href="Register.html">Register</a></li>
+              <li><a href="Contact Us.html">Contact Us</a></li>
+            </ul>
+            <div class="user-circle">U</div> 
+          </nav>
+
+        <h2>Register Now!!!</h2>
+        <p>Anemia is a major public health problem worldwide. Around 2.36 billion people, 30% of the 
+            world’s population, are affected by anemia and it is more prevalent among females (29.4%) 
+            compared to males (12.9%) (1). Hemoglobin concentration is used to assess the prevalence of 
+            anemia. Obesity also has been an epidemic in nearly every country in the world. 
+            Anthropometric measurements and lipid status are the best parameter in order to assess the 
+            magnitude of obesity.</p>
+
+    <section class=""container">
+    <form action="#" class="form">
+        <div class="input-box">
+            <label>Full Name(In block capitals):</label>
+            <input type="text">
         </div>
-        <div class="bottom">
-            <div class="form">
-                <h2>Want Emergency Estimate ?????</h2>
 
-                <label for="vBrand" class="form_label">Vehicle Brand</label>
-                <input type="text" id="vBrand" name="vBrand">
-
-                <label for="iType" class="form_label">Insurance Type</label>
-                <input type="text" id="iType" name="iType"><br>
-
-                <label for="vModel" class="form_label">Vehcile Model</label>
-                <input type="text" id="vModel" name="vModel">
-
-                <label for="vNo" class="form_label">Vehicle No</label>
-                <input type="text" id="vNo" name="vNo"><br>
-
-                <label for="yom" class="form_label">YOM</label>
-                <input type="text" id="yom" name="yom">
-
-                <label for="vType" class="form_label">Vehicle Type</label>
-                <input type="text" id="vType" name="vType"><br>
-
-                <label for="approx" class="approximation">Approximately Your Payment Is</label>
-                <label for="appox_payment" class="approximation">xxxxxxxx</label>
-
-                <p>**This provides approximate rate on your vehicle. These rates can be negotiate after inspecting your vehicle</p>
-            </div>   
+        <div class="input-box">
+            <label>Owner's NIC No:</label>
+            <input type="text">
         </div>
-        <script src="Script.js"></script>
+
+        <div class="input-box">
+            <label>Vehicle No:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Engine No:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Chassis No:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Vehicle Type:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Vehicle Model:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Year of Manufacture(YOM):</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Vehicle Number:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Vehicle Value:</label>
+            <input type="text">
+        </div>
+
+        <div class="input-box">
+            <label>Insurance Type(Third Party/Full):</label>
+            <input type="text">
+        </div>
+
+        <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+        
+       
+    </form>
+    </section>
+    <input type="checkbox" name="agree" class="checkbox">
+        <label for="agree" >I certify that the above information is true and that I agree to the terms & conditions mentiond in the insurance policy statement of the company.</label>
+
+                
+        <button type="submit" class="registerbtn">Register</button>
+    
     </body>
 </html>
     </body>
