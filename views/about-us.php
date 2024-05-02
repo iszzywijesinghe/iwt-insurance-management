eac4c6942e41d6181fec7671333fd8773f2682f9
sachstephperera <sachinthaisuru2001@gmail.com>
2024-05-02 10:19:04 +0530

    edited about-us and styles122

--- views/about-us.php
+++ views/about-us.php
@@ -1,51 +1,67 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <title>About Us</title>
-        <link rel="stylesheet" href="styles/style123.css" >
-    </head>
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="../asserts/css/style123.css">
+</head>
 
-    <body>
+<body>
 
-        <div class="heading">
-            <h1>About us</h1>
-            </div>
-        
-        <div class="imghcontainer">
-            <p class="imgtext">
-                We serve you the best Motor Insuarnce Solution
-            </p>
-        </div>
-
-        <div class="container">
-            <section class="about">
-                <div class="about-image">
-                    <img src="images/">
-                </div>
-                <div class="about-content">
-                    <h2>Our story</h2>
-                    <p>We are MotorGuard Insurance (PVT) Ltd. the largest state-owned insurer in Sri Lanka anchored to a pioneering legacy of financial stability and trust spanning over six decades.
-                        Established in 1997 as a state owned corporation and the pioneer insurer in the country, MotorGuard Insurance today manages an asset base of over LKR 313.2 billion which is the largest in the industry, the largest vehicle insurance fund in the local insurance industry amounting in excess of LKR 181.9 billion, and a LKR 6 billion strong capitalization, making us one of the most secure and reliable insurance solutions providers in the country.
-                        
-                    </p>
-                    <a href="" class="read-more">Read More</a>
-                </div>
-            </section>
-        </div>
-        
-        <div class="container1">
-            <h2>Our Mission</h2>
-            <p>We provide risk protection and life investment optimisation, that will deliver value to our customers through a team of committed professionals, whose contribution we recognise and value leading to superior shareholder value, whilst being a responsible corporate citizen</p>
-        </div>
-         <div class="container2">
-                    <h2 class="our-vision">Our Vision</h2>
-                    <p class="vparah">To be the Nation's first choice for risk Protection</p>
-        </div>
-    <div class="teamcontainer">
-
+    <div class="heading">
+        <h1>About us</h1>
     </div>
-    
+
+    <div class="imghcontainer">
+        <p class="imgtext">
+            We serve you the best Motor Insuarnce Solution
+        </p>
+    </div>
+
+    <div class="container">
+        <section class="about">
+            <div class="about-image">
+                <img src="images/">
+            </div>
+            <div class="about-content">
+                <h2>Our story</h2>
+                <p>We are MotorGuard Insurance (PVT) Ltd. the largest state-owned insurer in Sri Lanka anchored to a pioneering legacy of financial stability and trust spanning over six decades.
+                    Established in 1997 as a state owned corporation and the pioneer insurer in the country, MotorGuard Insurance today manages an asset base of over LKR 313.2 billion which is the largest in the industry, the largest vehicle insurance fund in the local insurance industry amounting in excess of LKR 181.9 billion, and a LKR 6 billion strong capitalization, making us one of the most secure and reliable insurance solutions providers in the country.
+
+                </p>
+                <!-- <a href="" class="read-more">Read More</a> -->
+            </div>
+        </section>
+    </div>
+    <div style="display: flex; flex-direction:row; margin-top:4%; gap: 30px; height:60%;">
+        <div style="width: 50%;;" class="box-border">
+            <h2 style="padding: 15px;" class="our-vision">Our Vision</h2>
+            <p class="vparah">To be the Nation's first choice for risk Protection</p>
+        </div>
+        <div style="width: 50%;" class="box-border">
+            <h2 style="text-align: center; margin-top: 10px;">Our Mission</h2>
+            <p style="text-align: center; margin-top: 10px;">We provide risk protection and life investment optimisation, that will deliver value to our customers through a team of committed professionals, whose contribution we recognise and value leading to superior shareholder value, whilst being a responsible corporate citizen</p>
+        </div>
+    </div>
+    <div class="teamcontainer">
+        <div style="display: flex; flex-direction: row; gap: 30px; align-items: center;">
+            <div style="background-color: red; width: 200px; height: 200px;">
+        <img src="" alt="">
+        <h4>>ceo</h4>
+        </div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Two</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Three</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Four</div>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 30px; align-items: center; margin-top: 10px;">
+            <div style="background-color: red; width: 200px; height: 200px;">Item One</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Two</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Three</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Four</div>
+            <div style="background-color: red; width: 200px; height: 200px;">Item Fove</div>
+        </div>
+    </div>
+
     <footer id="myfooter">
         <div class="containerf">
             <h3 class="footerhead">MotorGurad Insurance (PVT) Ltd</h3>
@@ -53,7 +69,7 @@
             <h5 class="footerbody">+94 77 333 4444/ +94 112 333 444</h5>
             <h5 class="footerbody">info@motoguard.insurance.lk</h5>
             <div class="footlogo">
-                    <img src="images/logo.png" alt="logo">
+                <img src="../asserts/img/logo.png" alt="logo">
             </div>
         </div>
         <div class="containerf2">
@@ -62,13 +78,13 @@
         </div>
 
     </footer>
-   
 
-        
-            
-        
-        
-    </body>
+
+
+
+
+
+</body>
 
 
 </html>
