f1a2bca906cc46bf130e09cf39d35a546025ade4
Kasunianupama <kasunianupama509@gmail.com>
2024-05-05 10:40:10 +0530

    insurance types services.php updated by kasuni

--- services/insurance-types-service.php
+++ services/insurance-types-service.php
@@ -4,7 +4,7 @@ include ("../utils/db-connection.php");
 function viewinsuranceTypes()
 {
     try {
-        $viewQuery = "SELECT * FROM insurance_types";
+        $viewQuery = "SELECT * FROM vehicle_insurance_type";
         $results = mysqli_query(getConnectionInstance(), $viewQuery);
         if (!$results) {
             $message = "Error Fetching Data" . mysqli_error(Connection());
@@ -17,12 +17,37 @@ function viewinsuranceTypes()
     }
 }
 
-function addinsuranceTypes()
+function addinsuranceTypes($cus_nic, $vehicle_no, $insurance_type, $price, $description)
 {
     try {
-        $insertQuery = "";
+        $insertQuery = "INSERT INTO vehicle_insurance_type (
+        cus_nic
+        insurance_type,
+        price,
+        vehicle_no,
+        description
+        ) VALUES (
+        '$cus_nic', 
+        '$insurance_type' , 
+        '$price' , 
+        '$vehicle_no', 
+        '$description 
+        )";
+
+        $result = mysqli_query(getConnectionInstance(), $insertQuery);
+
+        if (!$result) {
+
+            $message = "Error Inserting Data" . mysqli_error(getConnectionInstance());
+            echo "<script type='text/javascript'>alert('$message');</script>";
+
+        } else {
+            echo "<script type='text/javascript'>alert('Data Inserted Sucessfully');</script>";
+        }
+
     } catch (Exception $e) {
         echo $e->getMessage();
     }
+
 }
 ?>
