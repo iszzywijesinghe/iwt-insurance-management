9c46dafb6d516cbf20cb610de478d6c28a9d5e50
keshiniPaul <keshinipaul2020@gmail.com>
2024-05-05 10:47:50 +0530

    updated register service by keshini

--- services/register-service.php
+++ services/register-service.php
@@ -17,10 +17,43 @@ function viewuser()
     }
 }
 
-function adduser()
+
+    function adduser($cusnic, $firstname,	$lastname,	$email,	$houseno, $streetno, $city, $gender, $contactno)
 {
     try {
-        $insertQuery = "";
+        $insertQuery = "INSERT INTO customer (
+        cus_nic,
+        first_name,
+        last_name,
+        email,
+        house_no,
+        street_no,
+        city,
+        gender,
+        contact_no,		
+        ) VALUES ( 
+        '$cusnic' , 
+        '$firstname' , 
+        '$lastname', 
+        '$email',
+        '$houseno',
+        '$streetno',
+        '$city',
+        '$gender',
+        '$contactno'
+        )";
+
+        $result = mysqli_query(getConnectionInstance(), $insertQuery);
+
+        if (!$result) {
+
+            $message = "Error Inserting Data" . mysqli_error(getConnectionInstance());
+            echo "<script type='text/javascript'>alert('$message');</script>";
+
+        } else {
+            echo "<script type='text/javascript'>alert('Data Inserted Sucessfully');</script>";
+        }
+
     } catch (Exception $e) {
         echo $e->getMessage();
     }
