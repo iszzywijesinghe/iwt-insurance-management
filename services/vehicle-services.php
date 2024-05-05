65299acde98b2446a4dcb473fe255a419547527d
Isindu yuwaneka <isinduyuwaneka@gmail.com>
2024-05-05 15:46:34 +0530

    updated by isindu

--- services/vehicle-services.php
+++ services/vehicle-services.php
@@ -7,7 +7,7 @@ function viewVehicle()
         $viewQuery = "SELECT * FROM vehicle";
         $results = mysqli_query(getConnectionInstance(), $viewQuery);
         if (!$results) {
-            $message = "Error Fetching Data" . mysqli_error(Connection());
+            $message = "Error Fetching Data" . mysqli_error(getConnectionInstance());
             echo "<script type='text/javascript'>alert('$message');</script>";
         } else {
             return ($results);
