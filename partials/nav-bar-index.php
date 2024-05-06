96113feb5a4da2a481111ae3dc7077085694508e
Mahen Perera <kmaperera864@gmail.com>
2024-05-06 11:25:20 +0530

    linked top nav bar to pages

--- partials/nav-bar-index.php
+++ partials/nav-bar-index.php
@@ -4,10 +4,10 @@
     </div>
     <div>
     <ul>
-        <li><a href="Home.html">Home</a></li>
-        <li><a href="About Us.html">About Us</a></li>
-        <li><a href="Register.html">Register</a></li>
-        <li><a href="Contact Us.html">Contact Us</a></li>
+        <li><a href="views/index.php">Home</a></li>
+        <li><a href="views/about-us.php">About Us</a></li>
+        <li><a href="views/register-vehicle.php">Register</a></li>
+        <li><a href="views/contact-us.php">Contact Us</a></li>
     </ul>
 </div>
     <div class="user-circle">U</div> 
