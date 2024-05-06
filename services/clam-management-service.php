4f1081207781d0da4d24bb1fcb77af346a85640e
yohandevperera <yohanperera27@gmail.com>
2024-05-06 21:57:22 +0530

    completed update flow

--- services/clam-management-service.php
+++ services/clam-management-service.php
@@ -63,4 +63,32 @@ function getUpdateRow($id)
         echo $e->getMessage();
     }
 }
+
+function updateClaim($id, $vehicleNo, $vehiclePhotos, $date, $grade)
+{
+    try {
+        $updateQuery = "UPDATE claim 
+        SET 
+        vehicle_no='$vehicleNo', 
+        vehicle_photos='$vehiclePhotos',
+        date='$date',
+        accident_grade='$grade'
+        WHERE 
+        claim_id=$id";
+       
+       $result = mysqli_query(getConnectionInstance(), $updateQuery);
+
+        if (!$result) {
+
+            $message = "Error Inserting Data" . mysqli_error(getConnectionInstance());
+            echo "<script type='text/javascript'>alert('$message');</script>";
+
+        } else {
+            echo "<script type='text/javascript'>alert('Data Updated Sucessfully');</script>";
+        }
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 ?>
