122d6afbfdc3adfe5b73a9dcf5795b706527154b
Mahen Perera <kmaperera864@gmail.com>
2024-05-06 11:32:45 +0530

    edited linking of pages of top nav bar

--- partials/nav-bar-h.php
+++ partials/nav-bar-h.php
@@ -4,10 +4,10 @@
     </div>
     <div>
     <ul>
-        <li><a href="views/index.php">Home</a></li>
-        <li><a href="views/about-us.php">About Us</a></li>
-        <li><a href="views/register-vehicle.php">Register</a></li>
-        <li><a href="views/contact-us.php">Contact Us</a></li>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="about-us.php">About Us</a></li>
+        <li><a href="register-vehicle.php">Register</a></li>
+        <li><a href="contact-us.php">Contact Us</a></li>
     </ul>
 </div>
     <div class="user-circle">U</div> 
--- partials/nav-bar-index.php
+++ partials/nav-bar-index.php
@@ -4,7 +4,7 @@
     </div>
     <div>
     <ul>
-        <li><a href="views/index.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="views/about-us.php">About Us</a></li>
         <li><a href="views/register-vehicle.php">Register</a></li>
         <li><a href="views/contact-us.php">Contact Us</a></li>
