da101d23699831d03ef06a6c9ce1bcb31afff6f3
Isindu yuwaneka <isinduyuwaneka@gmail.com>
2024-05-07 00:35:07 +0530

    Updated by isindu

--- partials/user-dashboard-navbar-md.php
+++ partials/user-dashboard-navbar-md.php
@@ -3,6 +3,7 @@
     <ul class="nav nav-pills nav-stacked">
         <li class="active"><a href="#section1">Dashboard</a></li>
         <li><a href="../views/clam-management.php">Clam Management</a></li>
+        <li><a href="../views/vehicle-management.php">Vehicle Management</a></li>
         <li><a href="#section3">Gender</a></li>
         <li><a href="#section3">Geo</a></li>
     </ul><br>
