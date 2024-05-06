0e2c5413e0d23922c0929f9b9901081bfa8e89b6
Mahen Perera <kmaperera864@gmail.com>
2024-05-06 17:26:42 +0530

    updated about us

--- views/about-us.php
+++ views/about-us.php
@@ -9,100 +9,95 @@
 </head>
 
 <body>
-<?php include '../partials/nav-bar-h.php' ?>
+    <?php include '../partials/nav-bar-h.php' ?>
 
-<div class="image_hover">
-            <div class="img_container"> 
-                <img class="car_image" src = "../asserts/img/contact.png">
-            </div>
-            <div class="heading1">
+    <div class="image_hover">
+        <div class="img_container">
+            <img class="car_image" src="../asserts/img/contact.png">
+        </div>
+        <div class="heading1">
             <h2>About Us!</h2>
             <h1 class="des"></h1>
-             </div>
-</div>
-<div class="all-cont">
-    <div class="container">
-        <section class="about">
-            <div class="about-image">
-                <img src="">
+        </div>
+    </div>
+    <div class="all-cont">
+        <div class="container">
+            <section class="about">
+                <div class="about-image">
+                    <img src="">
+                </div>
+                <div class="about-content">
+                    <h2>Our story</h2>
+                    <p>We are MotorGuard Insurance (PVT) Ltd. the largest state-owned insurer in Sri Lanka anchored to a pioneering legacy of financial stability and trust spanning over six decades.
+                        Established in 1997 as a state owned corporation and the pioneer insurer in the country, MotorGuard Insurance today manages an asset base of over LKR 313.2 billion which is the largest in the industry, the largest vehicle insurance fund in the local insurance industry amounting in excess of LKR 181.9 billion, and a LKR 6 billion strong capitalization, making us one of the most secure and reliable insurance solutions providers in the country.
+
+                    </p>
+                    <!-- <a href="" class="read-more">Read More  </a> -->
+                </div>
+            </section>
+        </div>
+        <div style="display: flex; flex-direction:row; margin-top:4%; gap: 30px; ;">
+            <div style="width: 50%;;" class="box-border">
+                <div class="our-vision">
+                    <h2 style="padding: 15px;">Our Vision</h2>
+                    <p class="vparah">To be the Nation's first choice for risk Protection</p>
+                </div>
             </div>
-            <div class="about-content">
-                <h2>Our story</h2>
-                <p>We are MotorGuard Insurance (PVT) Ltd. the largest state-owned insurer in Sri Lanka anchored to a pioneering legacy of financial stability and trust spanning over six decades.
-                    Established in 1997 as a state owned corporation and the pioneer insurer in the country, MotorGuard Insurance today manages an asset base of over LKR 313.2 billion which is the largest in the industry, the largest vehicle insurance fund in the local insurance industry amounting in excess of LKR 181.9 billion, and a LKR 6 billion strong capitalization, making us one of the most secure and reliable insurance solutions providers in the country.
-
-                </p>
-                <!-- <a href="" class="read-more">Read More  </a> -->
+            <div style="width: 50%;" class="our-mission">
+                <h2 style="text-align: center; margin-top: 10px;">Our Mission</h2>
+                <p style="text-align: center; margin-top: 10px;">We provide risk protection and life investment optimisation, that will deliver value to our customers through a team of committed professionals, whose contribution we recognise and value leading to superior shareholder value, whilst being a responsible corporate citizen</p>
             </div>
-        </section>
-    </div>
-    <div style="display: flex; flex-direction:row; margin-top:4%; gap: 30px; ;">
-        <div style="width: 50%;;" class="box-border">
-        <div class="our-vision">
-            <h2 style="padding: 15px;">Our Vision</h2>
-            <p class="vparah">To be the Nation's first choice for risk Protection</p>
         </div>
-        </div>
-        <div style="width: 50%;" class="our-mission">
-            <h2 style="text-align: center; margin-top: 10px;">Our Mission</h2>
-            <p style="text-align: center; margin-top: 10px;">We provide risk protection and life investment optimisation, that will deliver value to our customers through a team of committed professionals, whose contribution we recognise and value leading to superior shareholder value, whilst being a responsible corporate citizen</p>
-        </div>
-    </div>
-    <div class="teamcontainer">
-        <div style="display: flex; flex-direction: row; gap: 30px; align-items: center;">
-            <div style="background-color:white; width: 200px; height: 200px;">
-        <img src="../asserts/img/CEO.jpg " alt="" width="200px" height="175px">
-        <h4 style="text-align: center;">CEO/Founder</h4>
-        </div>
-            <div style="background-color:white; width: 200px; height: 200px;">
-            <img src="../asserts/img/ceo_Co_Founder.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">CEO/Co Founder</h4>
-        </div>
-            <div style="background-color:white; width: 200px; height: 200px;">
-            <img src="../asserts/img/director1.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">Director Board Member</h4>
-        
-        </div>
-            <div style="background-color:white; width: 200px; height: 200px;">
-            <img src="../asserts/img/director2.avif" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">Director Board Member</h4>
-        
-        </div>
-        </div>
-        <div style="display: flex; flex-direction: row; gap: 30px; align-items: center; margin-top: 10px;">
-            <div style="background-color: white; width: 200px; height: 200px;">
-            <img src="../asserts/img/gm1.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">General Manager</h4>
-        
-        </div>
-            <div style="background-color: white; width: 200px; height: 200px;">
-            <img src="../asserts/img/gm2.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">General Manager</h4>
-        </div>
-            <div style="background-color: white; width: 200px; height: 200px;">
-            <img src="../asserts/img/bm_nug.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">Branch Manager
-        (Nugegoda)</h4>
-        </div>
-            <div style="background-color: white; width: 200px; height: 200px;">
-            <img src="../asserts/img/bm_ke.avif" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">Branch Manager
-        (Kegalle)
-        </div>
-            <div style="background-color: white; width: 200px; height: 200px;">
-            <img src="../asserts/img/bm_galle.jpg" alt="" width="200px" height="175px">
-            <h4 style="text-align: center;">Branch Manager
-            (Galle)</h4>
-        </div>
+        <div class="teamcontainer">
+            <div style="display: flex; flex-direction: row; gap: 30px; align-items: center;">
+                <div style="background-color:white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/CEO.jpg " alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">CEO/Founder</h4>
+                </div>
+                <div style="background-color:white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/ceo_Co_Founder.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">CEO/Co Founder</h4>
+                </div>
+                <div style="background-color:white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/director1.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">Director Board Member</h4>
+
+                </div>
+                <div style="background-color:white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/director2.avif" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">Director Board Member</h4>
+
+                </div>
+            </div>
+            <div style="display: flex; flex-direction: row; gap: 30px; align-items: center; margin-top: 10px;">
+                <div style="background-color: white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/gm1.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">General Manager</h4>
+
+                </div>
+                <div style="background-color: white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/gm2.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">General Manager</h4>
+                </div>
+                <div style="background-color: white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/bm_nug.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">Branch Manager
+                        (Nugegoda)</h4>
+                </div>
+                <div style="background-color: white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/bm_ke.avif" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">Branch Manager
+                        (Kegalle)
+                </div>
+                <div style="background-color: white; width: 200px; height: 200px;">
+                    <img src="../asserts/img/bm_galle.jpg" alt="" width="200px" height="175px">
+                    <h4 style="text-align: center;">Branch Manager
+                        (Galle)</h4>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-
-
-
-
     <?php include '../partials/footer-new.php' ?>
 </body>
-
 
 </html>
